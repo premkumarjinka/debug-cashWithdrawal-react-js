b6c239864ff4d73d4f04c1046805a2e9c1cfb6fa
premkumar <premkumar.jinka@gmail.com>
2023-03-01 07:42:45 +0000

    cash Withdrawl debug react js

---
+++ src/components/CashWithdrawal/index.js
@@ -0,0 +1,62 @@
+import {Component} from 'react'
+
+import DenominationItem from '../DenominationItem'
+
+import './index.css'
+
+class CashWithdrawal extends Component {
+  state = {
+    balance: 2000,
+  }
+
+  updateBalance = value => {
+    this.setState(prevState => ({balance: prevState.balance - value}))
+  }
+  // console.log("prem in comp")
+
+  render() {
+    const denominations = this.props
+    const {denominationsList} = denominations
+
+    const {balance} = this.state
+    const name = 'Sarah Williams'
+    const initial = name.slice(0, 1)
+    console.log('prem in ren')
+    console.log(denominationsList)
+    console.log(balance)
+
+    return (
+      <div className="app-container">
+        <div className="cash-withdrawal-container">
+          <div className="user-details-container">
+            <div className="initial-container">
+              <p className="initial">{initial}</p>
+            </div>
+            <p className="name">{name}</p>
+          </div>
+          <div className="balance-container">
+            <p className="your-balance">Your Balance</p>
+            <p className="balance">
+              {balance}
+              <br />
+              <span className="currency">In Rupees</span>
+            </p>
+          </div>
+          <p className="withdraw">Withdraw</p>
+          <p className="choose-sum">CHOOSE SUM (IN RUPEES)</p>
+          <ul className="denominations-list">
+            {denominationsList.map(eachDenomination => (
+              <DenominationItem
+                key={eachDenomination.id}
+                denominationDetails={eachDenomination}
+                updateBalance={this.updateBalance}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default CashWithdrawal
